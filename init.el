902de9d649fd8314d5c5d57de762fb526d42226f
wpc <alex.hal9000@gmail.com>
2011-09-25 16:04:17 -0700

    bump up font to 13.0, give find-file-in-project a shortcut

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 (package-initialize)
 
 
-;;(global-set-key (kbd "C-x f") 'find-file-in-project)
-;;(setq ruby-indent-level 2)
+(global-set-key (kbd "C-x f") 'find-file-in-project)
+(setq ruby-indent-level 2)
 
 ;;(custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -14,11 +14,11 @@
  ;; If there is more than one, they won't work right.
 ;; '(js2-cleanup-whitespace t)
 ;; '(js2-enter-indents-newline t))
-;;(custom-set-faces
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-;; '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
 
 ;;(tabbar-mode)
