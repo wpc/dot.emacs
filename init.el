4c8f3defbe9a30c928d732005f111b5f2d0ccc00
wpc <alex.hal9000@gmail.com>
2011-09-25 15:43:46 -0700

    blank init with starter-kit v2

---
+++ init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+;;(global-set-key (kbd "C-x f") 'find-file-in-project)
+;;(setq ruby-indent-level 2)
+
+;;(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(js2-cleanup-whitespace t)
+;; '(js2-enter-indents-newline t))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
+
+;;(tabbar-mode)
