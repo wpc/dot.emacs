1dc34ab3d937b61f4936b842dcfc941273046866
wpc <alex.hal9000@gmail.com>
2012-02-09 16:14:35 -0800

    * disable ido-ubiquiteous because it has problem with various commands such like (visit-tags-table) * js-indent-level is 2 now * add function to cd to textmate project root * add function to rgrep in textmate project root * add rhtml mode * add command to run ruby buffer

--- init.el
+++ init.el
@@ -14,7 +14,9 @@
  ;; If there is more than one, they won't work right.
  '(js2-cleanup-whitespace t)
  '(js2-indent-on-enter-key t)
- '(js2-enter-indents-newline t))
+ '(js2-enter-indents-newline t)
+ '(ido-ubiquitous-enabled nil)
+ '(js-indent-level 2))
 
 ;; increase font size
 (set-face-attribute 'default nil :height 130)
@@ -34,8 +36,6 @@
 (setq cssm-mirror-mode nil)
 
 
-
-
 (menu-bar-mode)
 (scroll-bar-mode)
 (tabbar-mode)
@@ -47,13 +47,63 @@
 (require 'smart-tab)
 (require 'extend-selection)
 (require 'select-text-in-quote)
+
+;; ruby-compilation
 (require 'ruby-compilation)
+(setenv "JAVA_OPTS" "-d32 -client")
+(setenv "JRUBY_OPTS" "--1.9")
+ 
+
+;; textmate
 (add-to-list 'load-path "~/.emacs.d/site-lisp/textmate.el")
 (require 'textmate)
 (textmate-mode)
+
+(defun is-rails-project ()
+  (when (textmate-project-root)
+    (file-exists-p (expand-file-name "config/environment.rb" (textmate-project-root)))))
+
+(defun cd-project-root (&optional args)
+  (interactive "P")
+  (cd (textmate-project-root)))
+
+(defun rgrep-in-project (regexp &optional files dir confirm)
+  "rgrep through textmate-project-root"
+   (interactive
+   (progn
+     (grep-compute-defaults)
+     (cond
+      ((and grep-find-command (equal current-prefix-arg '(16)))
+       (list (read-from-minibuffer "Run: " grep-find-command
+				   nil nil 'grep-find-history)))
+      (t (let* ((regexp (grep-read-regexp))
+		(files (grep-read-files regexp))
+		(dir (textmate-project-root))
+		(confirm (equal current-prefix-arg '(4))))
+	   (list regexp files dir confirm))))))
+   (rgrep regexp files dir confirm))
+
+(defun run-rails-test-or-ruby-buffer ()
+  (interactive)
+  (if (is-rails-project)
+      (let* ((path (buffer-file-name))
+             (filename (file-name-nondirectory path))
+             (test-path (expand-file-name "test" (textmate-project-root)))
+             (command (list ruby-compilation-executable "-I" test-path path)))
+        (pop-to-buffer (ruby-compilation-do filename command)))
+    (ruby-compilation-this-buffer)))
+
+(global-set-key [(super r)] 'run-rails-test-or-ruby-buffer)
+
+;; rvm
 (add-to-list 'load-path "~/.emacs.d/site-lisp/rvm.el")
 (require 'rvm)
 (rvm-use-default)
+
+
+;; rhtml
+(add-to-list 'load-path "~/.emacs.d/site-lisp/rhtml.git")
+(require 'rhtml-mode)
 
 ;; speedbar config
 (setq speedbar-show-unknown-files t)
@@ -64,7 +114,8 @@
         (add-to-list 'speedbar-frame-parameters '(left . 1190))
         (add-to-list 'speedbar-frame-parameters '(width . 25))
     )
-) 
+)
+
 
 
 (setq ruby-indent-level 2)
